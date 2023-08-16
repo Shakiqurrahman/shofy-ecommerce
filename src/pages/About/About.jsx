7a59876d9b306deff5c3687b4d7ccecd7b2cc4d7
learnwithancor <ancorpoint0@gmail.com>
2023-08-16 15:54:59 +0600

    cmtd

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
             <p className={story === true ? 'm-active' : ''} onClick={() => storyActive()}>FOUNDING STORY</p>
             <p className={press === true ? 'm-active' : ''} onClick={() => pressActive()}>Press</p>
           </div>
-        {mission &&<> <img src="/public/images/mission.jpg" alt="" />
+        {mission &&<> <img src="images/mission.jpg" alt="" />
         <p className="desc">
           Our mission at Holstee is to help conscious people (like you!) along
           your journey to live more fully and mindfully. In today’s complicated,
@@ -49,13 +49,13 @@ const About = () => {
           you.
         </p> </>}
         {story && <>
-        <img src="/public/images/story.jpg" alt="" />
+        <img src="images/story.jpg" alt="" />
         <p className="desc">
         In the summer of 2009, brothers Dave and Mike Radparvar decided to quit their jobs in the heat of the recession to go all-in on their passion project — Holstee, a functional and sustainable t-shirt company they had started with their friend Fabian Pfortmüller. Without a business plan or experience in fashion, they reasoned that in the worst-case scenario, it would be the most memorable summer of their lives. More than anything, Dave, Mike, and Fabian were looking to build a company that would allow them to live their dream. They wanted to create a company that aligned with their personal values and allowed them to have a positive impact on the world around them.
         </p>
         </>}
         {press && <>
-        <img src="/public/images/press.jpg" alt="" />
+        <img src="images/press.jpg" alt="" />
         <p className="desc">
         We are honored that others have found our work as exciting as we do! Here are some nice words from people that we look up to: “Most mission statements contain words like value and service but often fail to explain what the founders truly care about, much less inspire anyone else to care. Holstee's mission statement is an exception.”
         </p>
