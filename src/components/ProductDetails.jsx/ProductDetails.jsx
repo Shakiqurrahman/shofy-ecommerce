7eb22300addf41e872b31ecc9c4d2147bd549a54
learnwithancor <ancorpoint0@gmail.com>
2023-11-02 16:07:25 +0600

    commited

--- src/components/ProductDetails.jsx/ProductDetails.jsx
+++ src/components/ProductDetails.jsx/ProductDetails.jsx
@@ -1,24 +1,26 @@
 import React, { useContext } from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
-import { Link, useParams } from "react-router-dom";
+import {  useNavigate, useParams } from "react-router-dom";
 import { ShopContext } from "../../contexts/shop-context";
 import "./ProductDetails.css";
 const ProductDetails = () => {
   const { id } = useParams();
 
-  const { setClose, addToCart, products, cart } = useContext(ShopContext);
+  const {  addToCart, products, cart } = useContext(ShopContext);
 
   const detail = products.find((p) => p.id === Number(id));
 
 const exist = cart.find((x) => x.id === detail.id);
+
+const navigate = useNavigate();
 
 
   return (
     <>
       <div className="product-detail">
         <div className="detail-container">
-          <button onClick={() => setClose(false)} className="close-btn">
-           <Link to='/'> <AiOutlineCloseCircle /></Link>
+          <button onClick={ () => navigate(-1)} className="close-btn">
+          <AiOutlineCloseCircle />
           </button>
           <div className="productbox">
             <div className="img-box">
