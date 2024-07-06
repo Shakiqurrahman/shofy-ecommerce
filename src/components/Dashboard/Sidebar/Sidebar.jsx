bd8c783994b69b840fc7cf51ced721e5e2e6ad43
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-07-07 01:20:52 +0600

    sidebar added for sm

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcTwoSmartphones } from "react-icons/fc";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { IoBagAddSharp } from "react-icons/io5";
 import { MdDashboard, MdLogout } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+
 import { Link } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 import logo from "/logo.svg";
 
 const Header = () => {
+    const [activeSidebar, setActiveSidebar] = useState(false);
+
+    const handleToggle = () => {
+        setActiveSidebar(!activeSidebar);
+        console.log(activeSidebar);
+    }
     return (
         <aside className={styles.sidebar}>
-            <div className={styles.logo}>
-                <img src={logo} alt="Logo" />
+            <div className={styles.between}>
+                <div className={styles.logo}>
+                    <img src={logo} alt="Logo" />
+                </div>
+                <div className={` ${styles.hamburger}`} onClick={handleToggle}>
+                    {activeSidebar ? <RxCross2 /> : <GiHamburgerMenu />}
+                </div>
             </div>
-            <nav className={styles.navLinks}>
+            <nav className={`${activeSidebar && `${styles.showNavLinks}`} ${styles.navLinks}`}>
                 <ul>
                     <Link to="/admin/dashboard">
                         <li>
